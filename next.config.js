1248c99d182e70f3c6af3b11ed1dbb505499abca
Ryan Kmetz <rmkenv@gmail.com>
2026-06-23 13:33:59 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard.html',
+        permanent: false,
+      },
+    ]
+  },
   env: {
     GITHUB_RAW_BASE: process.env.GITHUB_RAW_BASE || 'https://raw.githubusercontent.com/rmkenv/climatewire/main/data',
   },
 }
-
 module.exports = nextConfig
